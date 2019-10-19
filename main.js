74673eacbe84567cc55fbec55b9d3deb152ed396
Igor Rodionov <henryshaw217@gmail.com>
2019-10-19 18:46:40 +0300

    multiple changes

--- main.js
+++ main.js
@@ -2,21 +2,54 @@
 
 const bars = document.querySelector('.bars');
 const showMenu = document.querySelector('.hide-menu');
-// const viewContainer = document.querySelector('.view-container');
-// const arrow = document.querySelector('.arrow');
-// const viewMore = document.querySelector('.view-more');
+const listContainer = document.querySelector('.bars-container__list');
+const viewContainer = document.querySelector('.view-container');
+const arrow = document.querySelector('.arrow');
+const viewMore = document.querySelector('.view-more');
+
+const sectionAbout = document.querySelector('.about');
+const sectionWork = document.querySelector('.work');
+const sectionContacts = document.querySelector('.contacts');
+
 
 bars.addEventListener('click', event => {
   event.currentTarget.classList.toggle('bars-close');
   showMenu.classList.toggle('hide-menu');
 })
 
-// viewContainer.addEventListener('mouseover', () => {
-//   arrow.style.display = 'none';
-//   viewMore.style.display = 'block';
-// })
+viewContainer.addEventListener('mouseover', () => {
+  arrow.style.display = 'none';
+  viewContainer.style.width = '20%';
+  viewMore.style.display = 'inline-block';
+})
 
-// viewContainer.addEventListener('mouseleave', () => {
-//   arrow.style.display = 'block';
-//   viewMore.style.display = 'none';
-// })
+viewContainer.addEventListener('mouseleave', () => {
+  arrow.style.display = 'inline-block';
+  viewMore.style.display = 'none';
+  viewContainer.style.width = '50px';
+})
+
+listContainer.addEventListener('click', event => {
+  if(event.target.classList.contains('bars-container__list')) {
+    console.log(event);
+    return;
+  }
+  event.preventDefault();
+  let top;
+
+  switch (event.target.dataset.id) {
+    case 'Обо мне':
+      top = sectionAbout.offsetTop;
+      break;
+    case 'Работы':
+      top = sectionWork.offsetTop;
+      break;
+    case 'Контакты':
+      top = sectionContacts.offsetTop;
+      break;
+  }
+  window.scrollTo({
+    top: top,
+    behavior: 'smooth'
+  })
+})
