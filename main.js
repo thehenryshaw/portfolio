0281c430948e2d68d020c40f801dafe507a7f896
Igor Rodionov <henryshaw217@gmail.com>
2019-10-21 15:22:56 +0300

    multiple changes

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-'use script';
+'use strict';
 
-const bars = document.querySelector('.bars');
-const showMenu = document.querySelector('.hide-menu');
-const listContainer = document.querySelector('.bars-container__list');
 const viewContainer = document.querySelector('.view-container');
 const arrow = document.querySelector('.arrow');
 const viewMore = document.querySelector('.view-more');
+const aboutDescription = document.querySelector('.about-description');
+const hideDescription = document.querySelector('.hide-description');
 
 const sectionAbout = document.querySelector('.about');
 const sectionWork = document.querySelector('.work');
 const sectionContacts = document.querySelector('.contacts');
-
-
-bars.addEventListener('click', event => {
-  event.currentTarget.classList.toggle('bars-close');
-  showMenu.classList.toggle('hide-menu');
-})
 
 viewContainer.addEventListener('mouseover', () => {
   arrow.style.display = 'none';
@@ -24,32 +17,11 @@ viewContainer.addEventListener('mouseover', () => {
 })
 
 viewContainer.addEventListener('mouseleave', () => {
-  arrow.style.display = 'inline-block';
+  arrow.style.display = 'block';
   viewMore.style.display = 'none';
   viewContainer.style.width = '50px';
 })
 
-listContainer.addEventListener('click', event => {
-  if(event.target.classList.contains('bars-container__list')) {
-    console.log(event);
-    return;
-  }
-  event.preventDefault();
-  let top;
-
-  switch (event.target.dataset.id) {
-    case 'Обо мне':
-      top = sectionAbout.offsetTop;
-      break;
-    case 'Работы':
-      top = sectionWork.offsetTop;
-      break;
-    case 'Контакты':
-      top = sectionContacts.offsetTop;
-      break;
-  }
-  window.scrollTo({
-    top: top,
-    behavior: 'smooth'
-  })
+viewMore.addEventListener('click', () => {
+  hideDescription.classList.toggle('hide-description');
 })
